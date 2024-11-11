8cc23849b23976d3cd4e39d5c51fbb6164f2959a
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-11-11 22:53:16 +0700

    finished

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.dicoding.ticketingapps"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.dicoding.ticketingapps"
